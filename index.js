1f1d5a63471e1a60fa5e020dc8830b8cf208b819
GandhyAmaya <gandhy.amaya@gmail.com>
2021-10-30 07:33:51 -0500

    se configura para hacer deploy con heroku

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
  const express = require('express');
  const routerApi = require('./routes');
+ const cors = require('cors');
  const{logErrors, errorHandler, boomErrorHandler} = require('./middlewares/errorHandler');
 
  const app = express();
- const port  = 3000;
+ const port  = process.env.PORT ||3000;
 
  app.use(express.json());
+ const whiteList = ['http://127.0.0.1:5500','https://myapp.co'];
+  const options = {
+    origin:(origin,callback)=>{
+      if(whiteList.includes(origin) || (!origin)){
+        callback(null,true);
+      }
+     else {
+        callback(new Error('no permitido'));
+      }
+    }
+  }
+  app.use(cors(options));
  app.get('/', (req, res)=>{
     res.send('hola mi server  en express');
  });
